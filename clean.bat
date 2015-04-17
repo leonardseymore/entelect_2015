2ce655c496ceebba50aed986807bd82eb9cc50a9
Leonard Seymore <leonardseymore@gmail.com>
2015-04-17 06:16:26 +0200

    - Bot.json added - Clean script added

---
+++ clean.bat
@@ -0,0 +1,4 @@
+rmdir /S /Q harness\player1
+rmdir /S /Q harness\player2
+rmdir /S /Q harness\Replays
+rm harness\match.log
--- run_harness.bat
+++ run_harness.bat
@@ -1,10 +1,11 @@
-
 rmdir /S /Q harness\player1
-xcopy /I src harness\player1
+xcopy /I /S src harness\player1
+copy src\bots\%1\bot.json harness\player1\bot.json
 echo python bots\%1\main.py %%1 > harness\player1\run.bat
  
 rmdir /S /Q harness\player2
-xcopy /I src harness\player2
+xcopy /I /S src harness\player2
+copy src\bots\%2\bot.json harness\player2\bot.json
 echo python bots\%2\main.py %%1 > harness\player2\run.bat
 
 cd harness
