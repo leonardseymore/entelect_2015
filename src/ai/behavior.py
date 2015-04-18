60578f7f7c9eeda4be4ee7999997843aa9ce1584
Leonard Seymore <leonardseymore@gmail.com>
2015-04-18 09:14:34 +0200

    - Behavior test cases added

--- src/ai/behavior.py
+++ src/ai/behavior.py
@@ -6,28 +6,28 @@ class Task():
     # child tasks
     children = []
 
-    def __init__(self):
-        return
+    def __init__(self, *children):
+        self.children = children
 
     # abstract method to be implemented
-    def run(self, blackboard):
+    def run(self, blackboard=None):
         return
 
 
 # selector runs children until a child evals to true
 class Selector(Task):
-    def run(self, blackboard):
+    def run(self, blackboard=None):
         for c in self.children:
-            if c.run():
+            if c.run(blackboard):
                 return True
         return False
 
 
 # sequence runs children until a child evals to false
 class Sequence(Task):
-    def run(self, blackboard):
+    def run(self, blackboard=None):
         for c in self.children:
-            if not c.run():
+            if not c.run(blackboard):
                 return False
         return True
 
@@ -43,33 +43,37 @@ class Decorator(Task):
 
 # limit number of times task can be executed
 class Limit(Decorator):
-    runLimit = 0
-    runSoFar = 0
+    run_limit = 0
+    run_so_far = 0
 
-    def run(self, blackboard):
-        if self.runSoFar >= self.runLimit:
+    def __init__(self, child, run_limit):
+        Decorator.__init__(self, child)
+        self.run_limit = run_limit
+
+    def run(self, blackboard=None):
+        if self.run_so_far >= self.run_limit:
             return False
-        self.runSoFar += 1
-        return self.child.run()
+        self.run_so_far += 1
+        return self.child.run(blackboard)
 
 
 # run a child task until it fails
 class UntilFail(Decorator):
-    def run(self, blackboard):
+    def run(self, blackboard=None):
         while True:
-            if not self.child.run():
+            if not self.child.run(blackboard):
                 break
         return True
 
 
 # flip the return value of task
 class Inverter(Decorator):
-    def run(self, blackboard):
-        return not self.child.run()
+    def run(self, blackboard=None):
+        return not self.child.run(blackboard)
 
 
 # inject blackboard into child task
 class BlackboardManager(Decorator):
-    def run(self, blackboard):
+    def run(self, blackboard=None):
         new_blackboard = Blackboard(blackboard)
         return self.child.run(new_blackboard)
---
+++ src/tests/ai/test_behavior.py
@@ -0,0 +1,104 @@
+import unittest
+from ai.behavior import *
+
+
+class TaskFail(Task):
+    def run(self, blackboard=None):
+        return False
+
+
+class TaskSucceed(Task):
+    def run(self, blackboard=None):
+        return True
+
+
+class TaskSucceedXTimes(Task):
+    times_succeed = 0
+    times_ran = 0
+
+    def __init__(self, times_succeed, *children):
+        Task.__init__(self, *children)
+        self.times_succeed = times_succeed
+
+    def run(self, blackboard=None):
+        result = False
+        if self.times_ran < self.times_succeed:
+            result = True
+        self.times_ran += 1
+        return result
+
+
+class TaskBlackboardSetValue(Task):
+    def run(self, blackboard=None):
+        blackboard.set('test_key', 'test_value')
+        return True
+
+
+class TaskBlackboardGetValue(Task):
+    def run(self, blackboard=None):
+        return blackboard.get('test_key') == 'test_value'
+
+
+class BehaviorTestCase(unittest.TestCase):
+    def test_children(self):
+        child1 = Task()
+        child2 = Task()
+        child3 = Task()
+        root = Task(child1, child2, child3)
+        self.assertEqual(root.children[0], child1)
+        self.assertEqual(root.children[1], child2)
+        self.assertEqual(root.children[2], child3)
+
+    def test_selector_fail(self):
+        child_fail = TaskFail()
+        selector = Selector(child_fail)
+        result = selector.run()
+        self.assertFalse(result)
+
+    def test_selector_succeed(self):
+        child_succeed = TaskSucceed()
+        selector = Selector(child_succeed)
+        result = selector.run()
+        self.assertTrue(result)
+
+    def test_sequence_fail(self):
+        child_fail = TaskFail()
+        sequence = Sequence(child_fail)
+        result = sequence.run()
+        self.assertFalse(result)
+
+    def test_sequence_succeed(self):
+        child_succeed = TaskSucceed()
+        sequence = Sequence(child_succeed)
+        result = sequence.run()
+        self.assertTrue(result)
+
+    def test_decorator_setup(self):
+        child = Task()
+        decorator = Decorator(child)
+        self.assertEquals(child, decorator.child)
+
+    def test_decorator_limit(self):
+        run_limit = 5
+        child = TaskSucceed()
+        limit = Limit(child, run_limit)
+        for i in range(0, 10):
+            result = limit.run()
+            if i < run_limit:
+                self.assertTrue(result)
+            else:
+                self.assertFalse(result)
+
+    def test_decorator_until_fail(self):
+        run_times = 5
+        child = TaskSucceedXTimes(run_times)
+        until_fail = UntilFail(child)
+        result = until_fail.run()
+        self.assertTrue(result)
+
+    def test_decorator_blackboard(self):
+        root = BlackboardManager(TaskBlackboardGetValue())
+        self.assertFalse(root.run())
+
+        root = BlackboardManager(Sequence(TaskBlackboardSetValue(), TaskBlackboardGetValue()))
+        self.assertTrue(root.run())
