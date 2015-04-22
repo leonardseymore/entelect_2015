1f2f5a43f9853850e16474c1fea84986408e30e4
Leonard Seymore <leonardseymore@gmail.com>
2015-04-22 08:44:46 +0200

    - RLE of game state added

--- src/experiments/alien_movement.py
+++ src/experiments/alien_movement.py
@@ -11,6 +11,10 @@
 #       AAA       #
 #                 # <-- Your building row
 
+------------------------------------------------------------------------------------------------------------------------
+CALCULATE POSSIBLE ALIEN WAVES reduce a pow(2,17) problem to 127 possibilities!
+------------------------------------------------------------------------------------------------------------------------
+
 Possible waves
 [ x  x  x  x  x  x]
 [    x  x  x  x  x]
@@ -32,6 +36,51 @@ Possible waves
 [                x]
 [                 ]
 [                 ] etc.
+
+------------------------------------------------------------------------------------------------------------------------
+POSSIBLE SYMBOLS IN EVERY ROW
+------------------------------------------------------------------------------------------------------------------------
+#                 # [XM!x|] = 5
+#       VVV       # [V!x|]  = 4
+# ---         --- # [i!x|-] = 5
+# ---         --- # [i!x|-] = 5
+# ---         --- # [i!x|-] = 5
+#                 # [i!x|]  = 4
+#                 # [i!x|]  = 4
+#                 # [i!x|]  = 4
+#                 # [i!x|]  = 4
+#                 # [i!x|]  = 4
+#          x  x  x# [i!x|]  = 4
+#                 # [i!|]   = 3
+#          x  x  x# [i!x|]  = 4
+#                 # [i!x|]  = 4
+#                 # [i!x|]  = 4
+#                 # [i!x|]  = 4
+#                 # [i!x|]  = 4
+#                 # [i!x|]  = 4
+# ---         --- # [i!x|-] = 5
+# ---         --- # [i!x|-] = 5
+# ---         --- # [i!x|-] = 5
+#       AAA       # [Aix|]  = 4
+#                 # [XMix|] = 5
+
+------------------------------------------------------------------------------------------------------------------------
+POSSIBLE BUILDING / SHIP LAYOUTS
+------------------------------------------------------------------------------------------------------------------------
+[                 ]
+[              MMM]
+[             MMM ]
+[            MMM  ]
+[           MMM   ]
+[          MMM    ] etc.
+
+[                 ]
+[              MMM]
+[             MMM ]
+[            MMM  ]
+[           MMMXXX]
+[    XXX   MMM    ] etc.
+
 """
 
 wave_forms = []
@@ -44,3 +93,40 @@ for i in range(1, 64):
 print '%d wave forms' % len(wave_forms)
 for wave in wave_forms:
     print '[%s]' % wave
+
+unit_forms = []
+unit_forms.append('                 ')
+for i in range(1, 16):
+    unit_forms.append(' ' * (i - 1) + '***' + ' ' * (17 - i - 2))
+
+print '%d unit forms' % len(unit_forms)
+for unit_form in unit_forms:
+    print '[%s]' % unit_form
+
+enemy_ship_forms = []
+missile_building_forms = []
+factory_building_forms = []
+building_forms = []
+for unit_form in unit_forms:
+    enemy_ship_forms.append(unit_form.replace('***', 'VVV'))
+    missile_building_forms.append(unit_form.replace('***', 'MMM'))
+    factory_building_forms.append(unit_form.replace('***', 'XXX'))
+    building_forms.append(unit_form.replace('***', 'MMM'))
+    building_forms.append(unit_form.replace('***', 'XXX'))
+
+
+for m in range(0, 15):
+    for f in range(0, 15):
+        if abs(m - f) >= 3:
+            row = list('                 ')
+            row[m] = 'M'
+            row[m + 1] = 'M'
+            row[m + 2] = 'M'
+            row[f] = 'X'
+            row[f + 1] = 'X'
+            row[f + 2] = 'X'
+            building_forms.append(''.join(row))
+
+print '%d building forms' % len(building_forms)
+for building_form in building_forms:
+    print '[%s]' % building_form
